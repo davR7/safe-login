99b732873327027df5bae991629e90e4308893d4
Davidson Ratis <davidsonmakarios@gmail.com>
2024-05-19 20:37:51 -0300

    created - Navbar component

---
+++ src/ui/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+import styles from "./navbar.module.css";
+import { options } from "./options";
+
+export default function Navbar() {
+  return (
+    <div className={`${styles.container} container`}>
+      <div className={styles.logo}>SafeLogin</div>
+      <nav className={styles.menu}>
+        <ul className={styles.options}>
+          {options?.map((opt) => (
+            <li className={styles.item} key={opt.id}>
+              <Link className={styles.link} href={opt.path}>
+                {opt.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </div>
+  );
+}
---
+++ src/ui/Navbar/options.ts
@@ -0,0 +1,5 @@
+export const options = [
+  { id: 1, path: "/", name: "Home" },
+  { id: 2, path: "/blog", name: "Blog" },
+  { id: 3, path: "/dashboard", name: "Dashboard" },
+];
