c21d0c5b2cdf22766362f3e0569e8fdbc2da2c9e
Davidson Ratis <davidsonmakarios@gmail.com>
2024-05-19 20:49:08 -0300

    created - mock user

---
+++ src/app/api/auth/[...nextauth]/data.ts
@@ -0,0 +1,9 @@
+export const user = {
+  id: "1",
+  name: "David Melo",
+  email: "david@test.com",
+  passsword: "12345",
+  image:
+    "https://images.pexels.com/photos/1036627/pexels-photo-1036627.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+  roles: ["USER", "ADMIN"],
+};
