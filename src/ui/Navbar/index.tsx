f62de5e87675db9864e63e24a9c1fe306ea8b9b0
Davidson Ratis <davidsonmakarios@gmail.com>
2024-05-20 20:20:29 -0300

    updated - display nav menu based on session status

--- src/ui/Navbar/index.tsx
+++ src/ui/Navbar/index.tsx
@@ -1,24 +1,30 @@
+"use client";
 import Link from "next/link";
 import styles from "./navbar.module.css";
 import { options } from "./options";
 import LogoutButton from "../LogoutButton/page";
+import { useSession } from "next-auth/react";
 
 export default function Navbar() {
+  const { status } = useSession();
+
   return (
     <div className={`${styles.container} container`}>
       <div className={styles.logo}>SafeLogin</div>
-      <nav className={styles.menu}>
-        <ul className={styles.options}>
-          {options?.map((opt) => (
-            <li className={styles.item} key={opt.id}>
-              <Link className={styles.link} href={opt.path}>
-                {opt.name}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <LogoutButton />
-      </nav>
+      {status === "authenticated" && (
+        <nav className={styles.menu}>
+          <ul className={styles.options}>
+            {options?.map(opt => (
+              <li className={styles.item} key={opt.id}>
+                <Link className={styles.link} href={opt.path}>
+                  {opt.name}
+                </Link>
+              </li>
+            ))}
+          </ul>
+          <LogoutButton />
+        </nav>
+      )}
     </div>
   );
 }
