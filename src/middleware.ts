be1846ba8bb8bea63929ad5d3269fb7b0221ba5b
Davidson Ratis <davidsonmakarios@gmail.com>
2024-05-22 13:32:25 -0300

    updated - dashboard page accessed only by ADMIN

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,24 @@
-export { default } from "next-auth/middleware";
+import { NextAuthMiddlewareOptions, NextRequestWithAuth, withAuth } from "next-auth/middleware";
+import { UserRole } from "./enums/UserRole";
+import { NextResponse } from "next/server";
 
-export const config = { matcher: ["/"] };
+const middleware = (req: NextRequestWithAuth) => {
+  const isAdmin = req.nextauth.token?.roles?.some(r => r === UserRole.ADMIN);
+  const isPagePrivate = req.nextUrl.pathname.startsWith("/dashboard");
+
+  if (isPagePrivate && !isAdmin) {
+    return NextResponse.rewrite(new URL("/denied", req.url));
+  }
+};
+
+const middlewareOptions: NextAuthMiddlewareOptions = {
+  callbacks: {
+    authorized({ token }) {
+      return !!token;
+    },
+  },
+};
+
+export default withAuth(middleware, middlewareOptions);
+
+export const config = { matcher: ["/", "/blog", "/dashboard"] };
