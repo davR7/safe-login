e9570b1bdabde9147ab0f42bed132ff3b40963df
Davidson Ratis <davidsonmakarios@gmail.com>
2024-05-22 13:25:27 -0300

    updated - home page finished

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <main className={`${styles.container} main`}>
       <h2>Home Page</h2>
+      <p>Bem-vindo de volta :)</p>
+      <Image src="/astronaut.gif" width={700} height={500} alt="astronaut" />
     </main>
   );
 }
