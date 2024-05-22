e922470fdf5e8c304bc1462f2f22d0399652a8ae
Davidson Ratis <davidsonmakarios@gmail.com>
2024-05-22 13:20:36 -0300

    updated - removed ADMIN role

--- src/app/api/auth/[...nextauth]/data.ts
+++ src/app/api/auth/[...nextauth]/data.ts
@@ -7,5 +7,5 @@ export const user = {
   passsword: "12345",
   image:
     "https://images.pexels.com/photos/1036627/pexels-photo-1036627.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-  roles: [UserRole.USER, UserRole.ADMIN],
+  roles: [UserRole.USER],
 };
