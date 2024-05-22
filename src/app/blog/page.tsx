d9affda0c33e2ab5ea3838764cf9d2f7277f699b
Davidson Ratis <davidsonmakarios@gmail.com>
2024-05-22 13:26:17 -0300

    updated - blog page finished

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -10,6 +10,7 @@ export default function Blog() {
   return (
     <main className={`${styles.container} container`}>
       <h2>Blog Page</h2>
+      <p>Você ainda não criou conteúdo...</p>
     </main>
   );
 }
